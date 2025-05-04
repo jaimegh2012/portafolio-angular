14c9d56777a3fe919a1e5f1f3e22c74d2e42be0f
Jaime Gutierrez <jaimegh_2012@hotmail.com>
2025-05-04 02:07:26 -0600

    template sin rutas

---
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio',
+  standalone: false,
+  templateUrl: './portafolio.component.html',
+  styleUrl: './portafolio.component.css'
+})
+export class PortafolioComponent {
+
+}
